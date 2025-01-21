bb577b0a095bd49e2bb13571fd479a80d7db3131
raschill <rebeccaaschilling@gmail.com>
2025-01-21 01:38:35 -0600

    reworked client folders

--- client/src/components/SignupForm.tsx
+++ client/src/components/SignupForm.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import type { ChangeEvent, FormEvent } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import {useMutation} from '@apollo/client';
 
-import { createUser } from '../utils/API';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 import type { User } from '../models/User';
 
@@ -14,6 +15,8 @@ const SignupForm = ({}: { handleModalClose: () => void }) => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -31,15 +34,33 @@ const SignupForm = ({}: { handleModalClose: () => void }) => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      // const response = await createUser(userFormData);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+
+      const {data} = await addUser({
+        variables: {
+          username: userFormData.username,
+          email: userFormData.email,
+          password: userFormData.password,
+        },
+      });
+
+      if (!data) {
+        throw new Error('Oopsie doodle');
       }
 
-      const { token } = await response.json();
+      const token = data.addUser.token;
+      localStorage.setItem('id_token', token);
       Auth.login(token);
-    } catch (err) {
+      handleModalClose();
+
+      }
+
+    //   const { token } = await response.json();
+    //   Auth.login(token);
+     catch (err) {
       console.error(err);
       setShowAlert(true);
     }
---
+++ client/src/utils/queries.ts
@@ -0,0 +1,17 @@
+import { gql } from '@apollo/client';
+
+export const GET_PLEASE = gql`
+  query Me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }}}`;
